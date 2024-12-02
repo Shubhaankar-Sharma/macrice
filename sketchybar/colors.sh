1a13fc1927618747427a567dbad4ca17e856725f
spongeboi <hi@spongeboi.com>
2024-12-02 12:31:48 -0800

    updates

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -13,9 +13,9 @@ export WHITE=0xffffffff
 # export ACCENT_COLOR=0xffffffff
 
 # -- Purple Scheme --
-# export BAR_COLOR=0xff211232
-# export ITEM_BG_COLOR=0xff211232
-# export ACCENT_COLOR=0xffc785ff
+export BAR_COLOR=0xff211232
+export ITEM_BG_COLOR=0xff211232
+export ACCENT_COLOR=0xffc785ff
 # export SELECTED_BG_COLOR=0xff211232
 # export BACKGROUND_1=0x603c3e4f
 # export BACKGROUND_2=0x60494d64
@@ -25,15 +25,15 @@ export WHITE=0xffffffff
 # export ACCENT_COLOR=0xffff2453
 
 # -- Blue Scheme --- 
-export BAR_COLOR=0xff021254
-export ITEM_BG_COLOR=0xff021254
-export ACCENT_COLOR=0xff15bdf9
-export SELECTED_BG_COLOR=0xff15bdf9
-export BACKGROUND_1=0x603c3e4f
-export BACKGROUND_2=0x60494d64
+# export BAR_COLOR=0xff021254
+# export ITEM_BG_COLOR=0xff021254
+# export ACCENT_COLOR=0xff15bdf9
+# export SELECTED_BG_COLOR=0xff15bdf9
+# export BACKGROUND_1=0x603c3e4f
+# export BACKGROUND_2=0x60494d64
 # -- Green Scheme --
 # export BAR_COLOR=0xff003315
-# export ITEM_BG_COLOR=0xff008c39
+# export ITEM_BG_COLOR=0xff003315
 # export ACCENT_COLOR=0xff1dfca1
 
 
